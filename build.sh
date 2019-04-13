1dda1f03ba0c7905bd104ab1f8514fde346c3280
Daniel Miranda <danielkza2@gmail.com>
2019-04-13 20:43:22 -0300

    Change file permissions for build to preserve cache

--- build.sh
+++ build.sh
@@ -1,22 +1,42 @@
 #!/usr/bin/env bash
 
-set -eo pipefail
+set -ex -o pipefail
 
 cmd="$1"
 shift
 
-case "$cmd" in
-build|push)
-    # The dash is important here
-    DOCKER_REV_TAG="-$(git rev-parse --short HEAD)"
-    export DOCKER_REV_TAG
+# The dash is important here
+DOCKER_REV_TAG="-$(git rev-parse --short HEAD)"
+export DOCKER_REV_TAG
+
+do_cmd() {
+    local cmd="$1"
+    shift
+
     docker-compose "$cmd" "$@"
 
-    if [ "$TAG_LATEST" -eq 1 ]; then
+    if [[ "$TAG_LATEST" -eq 1 ]]; then
         unset DOCKER_REV_TAG
         docker-compose "$cmd" "$@"
     fi
+}
+
+case "$cmd" in
+build)
+    # Make sure file permissions are uniform to avoid accidental cache busting
+    worktree=$(mktemp -d)
+    trap "rm -rf '$worktree'" EXIT
+
+    cp -a "${PWD}/." "${worktree}/"
+    chmod -R u=rwX,go=rX "$worktree"
+    cd "$worktree"
+
+    do_cmd build "$@"
+;;
+push)
+    do_cmd push "$@"
 ;;
 pull)
     ./pull-cache-from-images.py "$@"
+    docker-compose pull --ignore-pull-failures "$@"
 esac
